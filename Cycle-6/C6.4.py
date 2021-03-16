37d32041b3a60c8bbe7cfd5ffb84a96965b37838
nikhilshinukumar <74667418+nikhilshinukumar@users.noreply.github.com>
2021-03-16 15:52:39 +0530

    Add files via upload

---
+++ Cycle-6/C6.4.py
@@ -0,0 +1,9 @@
+#4.Write a Python program to read specific columns of a given CSV file and print the content of the columns.
+
+import csv
+with open('D:\depart.csv', newline='') as csvfile:
+ data = csv.DictReader(csvfile)
+ print("ID Department Name")
+ print("---------------------------------")
+ for row in data:
+   print(row['department_id'], row['department_name'])
